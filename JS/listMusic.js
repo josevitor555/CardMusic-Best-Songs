792dffed9e1804bef37888ae89110bfe69e90b12
josevitor555 <koservitor123@gmail.com>
2024-08-06 21:13:51 -0300

    CardMusic: Best Songs

---
+++ JS/listMusic.js
@@ -0,0 +1,24 @@
+var listMusic =  [
+  {
+    name: 'I like the way you kiss me',
+    artist: 'Artemes',
+    path: './music/Artemas - i like the way you kiss me (lyric video).mp3'
+  },
+  {
+    name: 'I am so crazy for youuu < 3',
+    artist: 'Rebzyyx',
+    path: './music/Im so crazy for youuu ᐸ_3.mp3'
+  },
+  {
+    name: 'All i want is you',
+    artist: 'Rebzyyx',
+    path: './music/Rebzyyx - all i want is you (Official Audio).mp3'
+  },
+  {
+    name: 'Ecstasy',
+    artist: 'SUICIDAL-IDOL',
+    path: './music/SUICIDAL-IDOL - ecstacy (slowed).mp3'
+  },
+]
+
+export default listMusic;
