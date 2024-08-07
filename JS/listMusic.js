cfb1de2ceb771574e671f4a71562e1c279b4fe6b
josevitor555 <koservitor123@gmail.com>
2024-08-07 20:00:58 -0300

    CardMusic 07/08/2024

--- JS/listMusic.js
+++ JS/listMusic.js
@@ -1,19 +1,34 @@
 var listMusic =  [
   {
-    name: 'I like the way you kiss me',
-    artist: 'Artemes',
-    path: './music/Artemas - i like the way you kiss me (lyric video).mp3'
+    name: 'Code Pandorum',
+    artist: 'wake up.kill.repeat Ft. Kid Bookie',
+    path: './music/Code_ Pandorum - wake up. kill. repeat Ft. Kid Bookie.mp3'
   },
   {
-    name: 'All i want is you',
-    artist: 'Rebzyyx',
-    path: './music/Rebzyyx - all i want is you (Official Audio).mp3'
+    name: 'Vantablack',
+    artist: 'Dirtyphonics & Sullivan King',
+    path: './music/Dirtyphonics & Sullivan King - Vantablack.mp3'
   },
   {
-    name: 'Ecstasy',
-    artist: 'SUICIDAL-IDOL',
-    path: './music/SUICIDAL-IDOL - ecstacy (slowed).mp3'
+    name: 'Misery',
+    artist: 'Evilwave',
+    path: './music/Evilwave - Misery.mp3'
   },
+  {
+    name: 'Cerberus',
+    artist: 'Kai Wachi',
+    path: './music/Kai Wachi - Cerberus.mp3'
+  },
+  {
+    name: 'Se7en Days',
+    artist: 'KROWW',
+    path: './music/KROWW - Se7en Days.mp3'
+  },
+  {
+    name: '28 Days Later',
+    artist: 'SYN',
+    path: './music/SYN - 28 Days Later.mp3'
+  }
 ]
 
 export default listMusic;
